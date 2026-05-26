beca4269c5ef097969361841d5b4c891894f2871
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2026-05-25 23:18:11 -0300

    feat(app): configure PrimeNG Aura theme, animations, and global styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: { darkModeSelector: false },
+      },
+    }),
+  ],
 };
